098db021c6c460fa07a2655aaf143de1704fdd65
Arjun Shanker <rjnshanker@gmail.com>
2013-05-12 23:00:52 -0500

    Twaltbot added functions, math doesn't quite work

--- SE2040 lab 8/bot/main.py
+++ SE2040 lab 8/bot/main.py
@@ -5,27 +5,21 @@ Created on May 12, 2013
 '''
 import sys
 import socket
+import re
 
+HOST = "irc.snoonet.org"
+PORT = 6667
 def main(argv=sys.argv):
     NICK = "Twaltbot"
     IDENT = "Twaltbot"
     REALNAME = "Twaltbot"
     readbuffer = ""
     connect = 0
-
-    if len(argv) == 2:
-        if isinstance(argv[0], int):
-            if "." in argv[1]:
-                HOST = argv[1]
-                PORT = argv[0]
-        elif isinstance(argv[1], int):
-            if "." in argv[0]:
-                HOST = argv[0]
-                PORT = argv[1]
-    else:        
-        HOST = "irc.snoonet.org"
-        PORT = 6667
-        
+    
+    if len(argv) == 3:
+        HOST = gethost(sys.argv)
+        PORT = getport(sys.argv)
+            
     sock = socket.socket();
     sock.connect((HOST, PORT))
     data = "NICK %s\r\n" % NICK
@@ -47,17 +41,46 @@ def main(argv=sys.argv):
             if line[0] == "PING":
                 pong = "PONG %s\r\n" % line[1]
                 sock.send(pong.encode())
-            if len(line) > 4:
-                if line[1]=='PRIVMSG' and line[3] == ':Twaltbot' and line[4] == 'hello':
+            if len(line) == 4:
+                if line[1]=='PRIVMSG' and line[3] == ':$help':
                     f = open("hello.txt", "r")
                     lineslist = f.readlines();
                     for words in lineslist:
                         output = "PRIVMSG %s :%s\r\n" % (line[2], words)
                         sock.send(output.encode())
+            if len(line) == 5:         
                 if line[1]=='PRIVMSG' and line[3] == ':$mathify':
                     evalthis = line[4]
+                    validMath(evalthis)
                     evalthis = eval(evalthis)
                     output = "PRIVMSG %s :%i\r\n" % (line[2], evalthis)
                     sock.send(output.encode())
+
+def gethost(argv):
+    if argv[1].isdigit():
+        if "." in argv[2]:
+            HOST = argv[2]
+    elif argv[2].isdigit():
+        if "." in argv[1]:
+            HOST = argv[1]
+    return HOST
+
+def getport(argv):
+    if argv[1].isdigit():
+        if "." in argv[2]:
+            PORT = int(argv[1])
+    elif argv[2].isdigit():
+        if "." in argv[1]:
+            PORT = int(argv[2])
+    return PORT
+    
+def validMath(arg):
+    numlist = re.split('[-/*+]+', arg)
+    charlist = re.split('[0123456789]+', arg)
+    for val in numlist:
+        
+    print(1)
+    print(numlist)
+    print(2)
                     
 main()
